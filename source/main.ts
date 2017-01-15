76bd1bd1851f0e54665e72ce657a050d63d82e19
Matt Hayes <matt@mysterycommand.com>
2017-01-15 17:42:11 -0500

    feat: use rxjs ts files directly, and build a little hello world

--- source/main.ts
+++ source/main.ts
@@ -1,1 +1,8 @@
-console.log('hello');
+import { Observable } from '@reactivex/rxjs/src/Observable';
+import '@reactivex/rxjs/src/add/observable/of';
+import '@reactivex/rxjs/src/add/operator/map';
+
+Observable
+    .of('hello', 'world')
+    .map((value: string) => value.split('').reverse().join(''))
+    .subscribe(console.log);

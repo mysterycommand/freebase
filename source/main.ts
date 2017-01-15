412489df9e701fd80f0bdfa9ccb17c4dde5efac6
Matt Hayes <matt@mysterycommand.com>
2017-01-15 18:50:01 -0500

    style: tslint wants double quotes all of a sudden

--- source/main.ts
+++ source/main.ts
@@ -1,8 +1,8 @@
-import { Observable } from '@reactivex/rxjs/src/Observable';
-import '@reactivex/rxjs/src/add/observable/of';
-import '@reactivex/rxjs/src/add/operator/map';
+import "@reactivex/rxjs/src/add/observable/of";
+import "@reactivex/rxjs/src/add/operator/map";
+import { Observable } from "@reactivex/rxjs/src/Observable";
 
 Observable
-    .of('hello', 'world')
-    .map((value: string) => value.split('').reverse().join(''))
+    .of("hello", "world")
+    .map((value: string) => value.split("").reverse().join(""))
     .subscribe(console.log);

821b21b7fde6ddfe541090239f9c9230fb397b50
Matt Hayes <matt@mysterycommand.com>
2017-01-17 09:37:14 -0500

    spike: more poking around minimal glimmer setup

--- spike.js
+++ spike.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
+
 const çcompiler = require('@glimmer/compiler');
 const çruntime = require('@glimmer/runtime');
 
-console.log(çcompiler);
-console.log(çruntime);
+const hbs = fs.readFileSync('./source/header.hbs', { encoding: 'utf8' });
+const precompiled = çcompiler.precompile(hbs);
+
+console.log(hbs);
+console.log(precompiled);
+console.log(çruntime.templateFactory(JSON.parse(precompiled)).create(this));

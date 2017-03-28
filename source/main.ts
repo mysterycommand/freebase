8430ca7bb5bbed6ebce252d00f95586ebf51de3d
Matt Hayes <mhayes@tumblr.com>
2017-03-28 11:52:30 -0700

    chore: remove everything

--- source/main.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from '@reactivex/rxjs/src/Observable';
-import '@reactivex/rxjs/src/add/observable/of';
-import '@reactivex/rxjs/src/add/operator/map';
-
-Observable
-    .of('hello', 'world')
-    .map((value: string) => value.split('').reverse().join(''))
-    .subscribe(console.log);
